8edb0dae58b9237bbcc6ea405041c45da2a7c112
Elias Romero <eliasrmr2004@gmail.com>
2024-01-03 14:00:35 -0600

    added html css and js file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("click", e => {
     let handle
     if (e.target.matches(".handle")) {
         handle = e.target
-    } 
+    }
     
     else {
         handle = e.target.closest(".handle")
@@ -24,3 +24,23 @@ function onHandleClick(handle) {
         slider.style.setProperty("--slider-index", sliderIndex + 1)
     }
 }
+
+
+console.log(scrollValue);
+   const scrollValue = window.scrollY;
+function changeBg(){
+    
+ 
+    // const navbar = document.getElementById('wrapper');
+
+    // if(scrollValue >= 5){
+    //     navbar.classList.add('bg-change-color');
+    // }
+
+    // else{
+    //     navbar.classList.remove('bg-change-color');
+    // }
+
+
+}
+window.addEventListener('scroll', changeBg);
